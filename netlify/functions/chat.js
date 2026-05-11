f545559f98eac42d768923205f5795c48775dfcd
Gowtham S <pg3259060@gmail.com>
2026-05-11 16:29:03 +0530

    removed latest model

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -69,7 +69,7 @@ exports.handler = async function (event) {
 
     // API Request
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`,
+     https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}
       {
         method: "POST",
 
